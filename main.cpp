b8a294e0ea75c3561b018b088b1ef369cbfe7ed7
FireHunter234 <FireHunter234@users.noreply.github.com>
2015-04-12 10:10:16 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "Schluesseltafel.h"
+
+using namespace std;
+
+int main(){
+	int testNumber = 9;
+	cout << testNumber << endl;
+	system("Pause");
+}
